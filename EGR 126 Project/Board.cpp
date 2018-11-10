fc65372aeaf184f174c6dc4c2d9e70e371b1ff0b
alvesketni <km28418@email.vccs.edu>
2018-11-10 12:40:58 -0500

    check board completed

--- EGR 126 Project/Board.cpp
+++ EGR 126 Project/Board.cpp
@@ -9,16 +9,7 @@ Board::Board() {
 
 bool Board::solved() {
 	
-//Board check.
-	//Check for sum on each box, column and box:
-	if(Sum[row] == 45 && Sum[col] == 45 && Sum[3 * 3] == 45) {
-		// do nothing - continue
-	}
-	else {
-		return false;
-	}
-
-
+	//Board check.
 	//Check duplicate numbers on each row:
 	for(int row = 0; row < 9; row++) {
 		int flag = 0;
@@ -37,36 +28,31 @@ bool Board::solved() {
 	}
 
 	//Check duplicate numbers on each column:
-	for(int col = 0; col < 9; col++) {
+	for (int col = 0; col < 9; col++) {
 		int flag = 0;
 
-		for(int row = 0; row < 9; row++)
-			if(flag & (1 << get(row, col)))
+		for (int row = 0; row < 9; row++) {
+			if (flag & (1 << get(row, col))) {
 				return false;
-
-			else
+			}
+			else {
 				flag |= (1 << get(row, col));
+			}
+		}
 	}
 
 	//Check duplicate numbers on each box:
-	for(int box = 0; box < 9; box++) {
-		for(int ofs = 0; ofs < 9; ofs++);
-		int col = (box % 3) * 3;
-		int row = ((int) (box / 3)) * 3;
-
-		if(flag & (1 << get(row, col)))
-			return false;
-
-		else
-			flag |= (1 << get(row, col));
-	}
-
 	for(int box_row = 0; box_row < 9; box_row += 3) {
 		for(int box_col = 0; box_col < 9; box_col += 3) {
 			int flag = 0;
 			for(int row = box_row; row < box_row + 3; box_row++) {
-				for(int col = box_col; col < box_col + 3; box_col++) {
-					// do checks
+				for (int col = box_col; col < box_col + 3; box_col++) {
+					if (flag & (1 << get(row, col))) {
+						return false;
+					}
+					else {
+						flag |= (1 << get(row, col));
+					}
 				}
 			}
 		}
